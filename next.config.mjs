e74a5416bb76262da93ce6fea22173ebf9226da1
Nida <nnpervez333@gmail.com>
2024-11-11 22:00:55 -0800

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['example.com'],
+    },
+    env: {
+      API_URL: process.env.API_URL,
+    },
+  };
+  
+  export default nextConfig;
+  
